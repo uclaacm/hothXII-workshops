1e0dbf8b9bbd8cfc94bf2a6ed8e63d5eef85d051
ltanggit <sendtolorelei@outlook.com>
2025-02-25 22:49:37 -0800

    added demo

---
+++ intro-to-databases/databases-demo/backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5001;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
